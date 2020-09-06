d04a3523a38a232f1d30f66e2e244ef3e6cb16b4
jyj <1036470143@qq.com>
2020-09-06 16:03:34 +0800

    进阶

---
+++ day04_Map/src/com/itheima/demo01/Map/Demo02KeySet.java
@@ -0,0 +1,53 @@
+package com.itheima.demo01.Map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/*
+    Map集合的第一种遍历方式:通过键2找值的方式
+    Map集合中的方法:
+        Set<K> keySet()返回此映射中包含的键的 Set 视图.
+    实现步骤:
+        1.使用Map集合中的方法keySet(),把Map集合所有的可以取出来,存储到一个Set集合中
+        2.遍历set集合,获取Map集合中的每一个key
+        3.通过Map集合中的方法get(key),通过key找到value
+ */
+public class Demo02KeySet {
+    public static void main(String[] args) {
+        //创建Map集合对象
+        Map<String, Integer> map = new HashMap<>();
+        map.put("林志玲", 178);
+        map.put("赵丽颖", 168);
+        map.put("杨颖", 165);
+
+        //1.使用Map集合中的方法keySet(),把Map集合所有的key取出来,存储到一个Set集合中
+        Set<String> set = map.keySet();
+
+        Iterator<String> it = set.iterator();
+
+        //2.遍历set集合,获取Map集合中的每一个key
+        //使用迭代器遍历Set集合
+        while (it.hasNext()) {
+            String value = it.next();
+            System.out.println(value + "=" + map.get(value));
+        }
+        System.out.println("-------------------------");
+
+        //使用增强for循环遍历Set集合
+        for (String s : set) {
+            //.3.通过Map集合中的方法get(key),通过key找到value
+            Integer value = map.get(s);
+            System.out.println(s + "=" + value);
+        }
+        System.out.println("-------------------------");
+
+        //使用增强for遍历Set集合
+        for (String s : map.keySet()) {
+            //3.通过Map集合中方法get(key),通过key找到value
+            Integer value = map.get(s);
+            System.out.println(s + "=" + value);
+        }
+    }
+}
