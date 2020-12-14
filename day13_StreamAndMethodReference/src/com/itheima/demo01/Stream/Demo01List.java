756d8dbe4894d9b9af12091c715286143e9e7377
jyj <1036470143@qq.com>
2020-12-14 15:03:18 +0800

    进阶

---
+++ day13_StreamAndMethodReference/src/com/itheima/demo01/Stream/Demo01List.java
@@ -0,0 +1,14 @@
+package com.itheima.demo01.Stream;
+
+/**
+ * @Description：使用传统的方式,遍历集合,对集合中的数据进行过滤
+ * @Author 金宇佳
+ * @Date 2020/12/14 15:02
+ * @Version 1.0
+ */
+/*
+    使用传统的方式,遍历集合,对集合中的数据进行过滤
+ */
+public class Demo01List {
+
+}
